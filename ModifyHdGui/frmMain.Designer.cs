fb93545fd6e39735a0432a26d62da767e287d859
William Krebbers <william.krebbers@gmail.com>
2016-10-12 19:20:10 +0200

    Add project files.

---
+++ ModifyHdGui/frmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ModifyHdGui
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.udSize = new System.Windows.Forms.NumericUpDown();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openVHDFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.lbSize = new System.Windows.Forms.Label();
+            this.lbPath = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.udSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // udSize
+            // 
+            this.udSize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.udSize.Location = new System.Drawing.Point(76, 38);
+            this.udSize.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.udSize.Name = "udSize";
+            this.udSize.Size = new System.Drawing.Size(396, 20);
+            this.udSize.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(397, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openVHDFileDialog
+            // 
+            this.openVHDFileDialog.Filter = "VirtualBox HD Files (*.vdi)|*.vdi";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(76, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(315, 20);
+            this.txtPath.TabIndex = 2;
+            // 
+            // btnResize
+            // 
+            this.btnResize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResize.Location = new System.Drawing.Point(397, 173);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(75, 23);
+            this.btnResize.TabIndex = 3;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // lbSize
+            // 
+            this.lbSize.AutoSize = true;
+            this.lbSize.Location = new System.Drawing.Point(12, 40);
+            this.lbSize.Name = "lbSize";
+            this.lbSize.Size = new System.Drawing.Size(58, 13);
+            this.lbSize.TabIndex = 4;
+            this.lbSize.Text = "Size (MB) :";
+            // 
+            // lbPath
+            // 
+            this.lbPath.AutoSize = true;
+            this.lbPath.Location = new System.Drawing.Point(12, 15);
+            this.lbPath.Name = "lbPath";
+            this.lbPath.Size = new System.Drawing.Size(32, 13);
+            this.lbPath.TabIndex = 5;
+            this.lbPath.Text = "Path:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(76, 65);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(396, 102);
+            this.txtOutput.TabIndex = 6;
+            this.txtOutput.WordWrap = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 205);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lbPath);
+            this.Controls.Add(this.lbSize);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.udSize);
+            this.MinimumSize = new System.Drawing.Size(500, 150);
+            this.Name = "frmMain";
+            this.Text = "Resize HD GUI";
+            ((System.ComponentModel.ISupportInitialize)(this.udSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown udSize;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openVHDFileDialog;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Label lbSize;
+        private System.Windows.Forms.Label lbPath;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ ModifyHdGui/frmMain.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace ModifyHdGui
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if(openVHDFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = openVHDFileDialog.FileName;
+            }
+
+            txtOutput.Clear();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe",
+                Arguments = string.Format("showmediuminfo disk \"{0}\"", txtPath.Text),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false
+            };
+
+            try
+            {
+                using (Process cmd = Process.Start(startInfo))
+                {
+                    txtOutput.Text += "Disk Info:\r\n";
+
+                    using (StreamReader errorReader = cmd.StandardError)
+                    {
+                        string error = errorReader.ReadToEnd();
+                        txtOutput.Text += error;
+                    }
+
+                    using (StreamReader outputReader = cmd.StandardOutput)
+                    {
+                        string output = outputReader.ReadToEnd();
+                        txtOutput.Text += output;
+                    }
+                }
+
+                foreach (string line in txtOutput.Lines)
+                {
+                    if (line.StartsWith("Capacity"))
+                    {
+                        this.udSize.Value = Int32.Parse(line.Split(' ')[7]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtOutput.Text += "Invalid file!\r\n";
+            }
+            
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe",
+                Arguments = string.Format("modifyhd \"{0}\" --resize {1}", txtPath.Text, udSize.Value.ToString()),
+                RedirectStandardOutput = true,
+                RedirectStandardError  = true,
+                UseShellExecute        = false
+            }; 
+
+            try
+            {
+                using (Process cmd = Process.Start(startInfo))
+                {
+                    txtOutput.Text += "Starting with resizing...\r\n";
+
+                    using (StreamReader errorReader = cmd.StandardError)
+                    {
+                        string error = errorReader.ReadToEnd();
+                        txtOutput.Text += error;
+                    }
+
+                    using (StreamReader outputReader = cmd.StandardOutput)
+                    {
+                        string output = outputReader.ReadToEnd();
+                        txtOutput.Text += output;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.Source);
+
+            }
+
+        }
+
+    }
+}
